7603b28935c85e4e9d284e8a0bd8ec5edfd4e556
Imaizumi <y.imaizumi86@gmail.com>
2026-02-07 10:22:03 +0900

    Remove unused Hono files

--- src/lib/client.ts
+++
@@ -1,8 +0,0 @@
-// src/lib/client.ts
-
-import { hc } from 'hono/client'
-import type { AppType } from '@/server'
-
-// Hono RPC Client
-// URLはデプロイ環境に合わせて書き換わるようにwindow.locationを使うか、相対パスで指定
-export const client = hc<AppType>('/')
